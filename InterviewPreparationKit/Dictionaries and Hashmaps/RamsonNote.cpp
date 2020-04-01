d464198f6d84b4c9b1ef134367c0d2dfe69e2309
Iliya <55933990+CoDipper@users.noreply.github.com>
2020-04-01 04:08:39 +0300

    Create RamsonNote.cpp

---
+++ InterviewPreparationKit/Dictionaries and Hashmaps/RamsonNote.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <map> 
+
+using namespace std;
+
+vector<string> split_string(string);
+
+// Complete the checkMagazine function below.
+bool checkMagazine(vector<string> magazine, vector<string> note) {
+    unordered_map<string, int> mag;
+    for (string x: magazine) {
+        if (mag.count(x)) {
+            mag[x] ++;
+        } else {
+           mag[x] = 1;
+        }
+    }
+    for (string x: note) {
+        if (mag.count(x)) {
+            mag[x] --;
+            if (mag[x] < 0) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    //some inPut...
+    if(checkMagazine(magazine, note)){
+        cout<<"Yes";
+    }else{
+        cout<<"No";
+    }
+    return 0;
+}
